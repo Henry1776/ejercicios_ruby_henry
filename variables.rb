a5d7a1223c3dff03ef641c08cd2a6d703fe4303d
Henry Artavia <hernyartavia767@hotmail.com>
2021-01-26 17:03:44 -0600

    strings

---
+++ variables.rb
@@ -0,0 +1,12 @@
+#ruby  puede tener numero enteros
+puts "enteros"
+a = 37 
+a + 1 
+puts a
+a = a + 1
+puts a
+#ruby  puede tener decimales
+b = 2.5
+c = 1
+puts "decimales"
+puts b + c #el resultado de un entero mas un decimal es decimal
